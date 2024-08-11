4f2f9e08e432492c371f3810d6affce19552f7c3
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2024-08-11 11:10:25 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/interactive_rating_component/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.interactive_rating_component
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:interactive_rating_component/screens/main_screen.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: MainScreen(),
+      ),
+    ),
+  );
+}
